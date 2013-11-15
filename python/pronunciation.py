295b89960dc4bdcceafb7b1501d4e32ec848ebc7
hg <hg@localhost>
2013-11-15 12:14:54 +1100

    Added python script to parse typesetting text looking for pronunciations.

---
+++ python/pronunciation.py
@@ -0,0 +1,73 @@
+
+from nltk.tokenize import RegexpTokenizer
+
+
+
+
+
+def tokenize_text(text):
+    """Return a list of tokens from this text"""
+    
+    
+    tokenizer = RegexpTokenizer(r"\\(\w|,)+|--|\w+|\[[a-z0-9.]+\]|[/()]")
+
+    return tokenizer.tokenize(text)
+
+def next_def(tokens):
+    """An iterator over definitions in this token stream.
+    Yields the next definition block as a sequence of tokens"""
+    
+    defblock = []
+    for tok in tokens:
+        if starts_definition(tok):
+            if defblock != []:
+                result = defblock
+                defblock = []
+                yield result
+                
+        defblock.append(tok)
+    
+def starts_definition(token):
+    """Return true if this token can start a definition"""
+    
+    return token in ['--', '[j111]']
+
+
+def process_definition(tokens):
+    """Given a definition, return the headword and the pronunciation"""
+    
+    if not starts_definition(tokens[0]):
+        return []
+    
+    headword = []
+    pron = []
+    in_pron = False
+    done_hw = False
+    for tok in tokens[1:]:
+        if tok == '/':
+            done_hw = True
+            in_pron = not in_pron
+            pron.append(tok)
+        if not done_hw:
+            headword.append(tok)
+        elif in_pron:
+            pron.append(tok)
+    
+    return (headword, pron)
+    
+    
+
+if __name__=='__main__':
+    
+    import sys
+    textfile = sys.argv[1]
+    h = open(textfile)
+    text = h.read()
+    h.close()
+    
+    tokens = tokenize_text(text)
+    
+    for defn in next_def(tokens):
+        print process_definition(defn)
+        
+        
